c619c4a2cde8bc088bae7aace6c682ae5c5d73d7
Sam Avery <sam.avery1@gmail.com>
2022-09-13 09:37:19 -0500

    linking js file and testing new js updates

--- index.js
+++ index.js
@@ -8,11 +8,22 @@
 
 //CLICK EVENT 
 
+const produceLists = document.getElementsByClassName("produce-list")
+const produceItems = Array.from(document.produceLists.getElementsByTagName("li"))
+const formText = document.querySelector("search-form")
+
+
+function handleClick(e){
+    formText.innerHTML = e.target.produceItems.value
+    }
+
+
+produceItems.forEach(listItem => {
+    listItem.addEventListener('click', handleClick())
+})
 
 
 
-
-
-
+handleClick()
 
 
