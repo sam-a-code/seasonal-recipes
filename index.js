17736c27c71ad09ebc67cfdb3b9d1aa6c08b7c97
Daniel Boley <danboley1@gmail.com>
2022-09-14 10:41:17 -0400

    fetch is working

--- index.js
+++ index.js
@@ -1,96 +1,77 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '855ea0d734mshbf36e3ce34a512fp105f93jsndd216aa16782',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "855ea0d734mshbf36e3ce34a512fp105f93jsndd216aa16782",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
 };
 
-// fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=${formText.value}`, options)
-// //fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=3", options)
-// 	.then(response => response.json())
-//   .then(data => console.log(data))
-// 	//.then(data => data.forEach(recipe => {renderRecipe(recipe)}))
-// 	.catch(err => console.error(err));
-
+const baseUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=";
 
 
 //TOGGLE BELOW
 
-
 const seasons = document.querySelectorAll(".season");
-seasons.forEach((season) => {season.addEventListener("click", (e) => {
-    season.lastElementChild.toggleAttribute('hidden')
-})});
+seasons.forEach((season) => {
+  season.addEventListener("click", (e) => {
+    season.lastElementChild.toggleAttribute("hidden");
+  });
+});
 
+//CLICK EVENT
 
+const seasonsDiv = document.querySelector("#seasons");
+const produceLists = document.getElementsByClassName(".produce-list");
+const produceItems = Array.from(document.getElementsByTagName("li"));
+const formText = document.querySelector("#search-form-input");
+const formButton = document.querySelector("#search-form");
+const recipeDiv = document.getElementById("recipes");
 
-
-//CLICK EVENT 
-
-const seasonsDiv = document.querySelector("#seasons")
-const produceLists = document.getElementsByClassName(".produce-list")
-const produceItems = Array.from(document.getElementsByTagName("li"))
-const formText = document.querySelector("#search-form-input")
-const formButton = document.querySelector("#search-form")
-const recipeDiv = document.getElementById("recipes")
-
-function handleClick(e){
-    formText.value = e.target.textContent
-     console.log(formText.value)
-
-     fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=${formText.value}`, options)
-    //fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=3", options)
-	  .then(response => response.json())
-    .then(data => console.log(data))
-	  //.then(data => data.forEach(recipe => {renderRecipe(recipe)}))
-	  .catch(err => console.error(err));
+function handleClick(e) {
+  formText.value = e.target.textContent;
+  console.log(formText.value);
 }
 
+produceItems.forEach((listItem) => {
+  listItem.addEventListener("click", (e) => handleClick(e));
+});
 
-produceItems.forEach(listItem => {
-    listItem.addEventListener('click', (e) => 
-    handleClick(e))
-})
-
-
-
-
-
-
-function handleForm(){
-    formButton.addEventListener('submit', (e) => 
-    e.preventDefault())
+function getRecipes() {
+  return fetch(`${baseUrl}${formText.value}`, options).then((response) =>
+    response.json()
+  );
 }
 
-function renderRecipe(){
-    const recipeName = document.createElement("h4")
-    const recipeImageURL = document.createElement("h6")
-    const recipeDescription = document.createElement("h5")
-//turned values into strings to test code, need to change back
-    recipeName.textContent = "recipe.name"
-    recipeImageURL.src = "recipe.thumbnail_url"
-    recipeDescription.textContent = "recipe.description"
-
-    const newRecipeCard = {
-        name: recipeName,
-        thumbnail_url: recipeImageURL,
-        description: recipeDescription
-    }
-
-    recipeDiv.append(newRecipeCard)
-
-    console.log(newRecipeCard)
+function handleForm() {
+  formButton.addEventListener("submit", (e) => {
+    e.preventDefault();
+    getRecipes()
+      .then((recipes) => {
+        recipes.results.forEach((recipe) => {
+          renderRecipe(recipe);
+        });
+      })
+      .catch((err) => console.error(err));
+  });
 }
 
-renderRecipe()
+handleForm();
 
-// RECIPE RETURNS
+function renderRecipe(recipe) {
+  const recipeName = document.createElement("h4");
+  const recipeImageURL = document.createElement("img");
+  const recipeDescription = document.createElement("h5");
 
-// let newRecipe = ''
+  recipeName.textContent = recipe.name;
+  recipeImageURL.src = recipe.thumbnail_url;
+  recipeDescription.textContent = recipe.description;
 
-// let newRecipeCard = {
-//     name: "",
-//     thumbnail_url: "",
-//     description: ""
-// }
+  // const newRecipeCard = {
+  //     name: recipeName,
+  //     thumbnail_url: recipeImageURL,
+  //     description: recipeDescription
+  // }
+
+  recipeDiv.append(recipeName, recipeImageURL, recipeDescription);
+}
+
