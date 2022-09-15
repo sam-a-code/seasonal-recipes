e165d334aad08ee702fd00fc8c4e80927006f858
Sam Avery <sam.avery1@gmail.com>
2022-09-15 08:48:19 -0500

    adding api js

---
+++ api.js
@@ -0,0 +1,9 @@
+const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "855ea0d734mshbf36e3ce34a512fp105f93jsndd216aa16782",
+      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+    },
+  };
+
+  
